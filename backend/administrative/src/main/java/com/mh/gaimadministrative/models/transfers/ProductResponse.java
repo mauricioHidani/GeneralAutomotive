6ae1e47de3209d746531608d034cbe0a98998a06
Mauricio Mityo Hidani <mauricio.hidani.work@gmail.com>
2024-02-27 15:13:58 -0300

    create product request and response

---
+++ backend/administrative/src/main/java/com/mh/gaimadministrative/models/transfers/ProductResponse.java
@@ -0,0 +1,52 @@
+package com.mh.gaimadministrative.models.transfers;
+
+import com.mh.gaimadministrative.models.entities.Product;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+public record ProductResponse(
+    String id,
+    String title,
+    String description,
+    Double charge,
+    Double weight,
+    Double space,
+    BigDecimal unitCost
+) {
+    public static Product toEntity(ProductResponse response) {
+        return new Product(
+                UUID.fromString(response.id()),
+                response.title(),
+                response.description(),
+                response.charge(),
+                response.weight(),
+                response.space(),
+                response.unitCost()
+        );
+    }
+
+    public static ProductResponse toResponse(Product entity) {
+        return new ProductResponse(
+                entity.getId().toString(),
+                entity.getTitle(),
+                entity.getDescription(),
+                entity.getCharge(),
+                entity.getWeight(),
+                entity.getSpace(),
+                entity.getUnitCost()
+        );
+    }
+
+    public static ProductResponse toResponse(ProductRequest request) {
+        return new ProductResponse(
+                request.id(),
+                request.title(),
+                request.description(),
+                request.charge(),
+                request.weight(),
+                request.space(),
+                request.unitCost()
+        );
+    }
+}
