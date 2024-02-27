d8db94ac60cc7115de868c9b6b617462f59105fd
Mauricio Mityo Hidani <mauricio.hidani.work@gmail.com>
2024-02-27 11:52:09 -0300

    get office enum by string

--- backend/administrative/src/main/java/com/mh/gaimadministrative/models/enums/Offices.java
+++ backend/administrative/src/main/java/com/mh/gaimadministrative/models/enums/Offices.java
@@ -1,7 +1,18 @@
 package com.mh.gaimadministrative.models.enums;
 
 public enum Offices {
+
     BUYING_COORDINATOR,
     LOGISTICS_ADMINISTRATOR,
-    DEMAND_PLANNING_SPECIALIST,
+    DEMAND_PLANNING_SPECIALIST;
+
+    public Offices toEnum(String str) {
+        for (Offices office : Offices.values()) {
+            if (office.toString().equals(str)) {
+                return office;
+            }
+        }
+        throw new IllegalArgumentException("The information for type Office validation is not valid");
+    }
+
 }
