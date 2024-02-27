6ac43b4c55075ede9723bd550c34d710f502b315
Mauricio Mityo Hidani <mauricio.hidani.work@gmail.com>
2024-02-27 15:12:42 -0300

    create order repository

---
+++ backend/administrative/src/main/java/com/mh/gaimadministrative/repositories/OrderRepository.java
@@ -0,0 +1,11 @@
+package com.mh.gaimadministrative.repositories;
+
+import com.mh.gaimadministrative.models.entities.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, UUID> {
+}
