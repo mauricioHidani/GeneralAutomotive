4caf36646b25065d0f507b4a97b3df9f843aae13
MauricioHidani <mauricio.hidani.work@gmail.com>
2024-03-06 16:54:46 -0300

    feat: administrator config adapter

--- backend/administrative/src/main/java/com/mh/ga/administrative/services/administrator/configs/AdministratorConfig.java
+++ backend/administrative/src/main/java/com/mh/ga/administrative/services/administrator/configs/AdministratorConfig.java
@@ -1,8 +1,21 @@
 package com.mh.ga.administrative.services.administrator.configs;
 
+import com.mh.ga.administrative.models.entities.Administrator;
+import com.mh.ga.administrative.repositories.AdministratorRepository;
+import com.mh.ga.administrative.repositories.adapter.AdministratorAdapter;
+import com.mh.ga.administrative.repositories.adapter.impls.AdministratorAdapterImpl;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.UUID;
 
 @Configuration
 public class AdministratorConfig {
+
+    @Bean
+    public AdministratorAdapter<Administrator, UUID> adapter(
+            AdministratorRepository repository) {
+        return new AdministratorAdapterImpl(repository);
+    }
 
 }
