063522d1c7e3b2825655991d05219741da0b5e61
MauricioHidani <mauricio.hidani.work@gmail.com>
2024-03-06 17:09:30 -0300

    feat: administrator controller use case save

--- backend/administrative/src/main/java/com/mh/ga/administrative/controllers/AdministratorController.java
+++ backend/administrative/src/main/java/com/mh/ga/administrative/controllers/AdministratorController.java
@@ -1,7 +1,11 @@
 package com.mh.ga.administrative.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.mh.ga.administrative.models.transfers.AdministratorRequest;
+import com.mh.ga.administrative.models.transfers.AdministratorResponse;
+import com.mh.ga.administrative.services.administrator.AdministratorService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 @RequestMapping(
@@ -9,5 +13,22 @@ import org.springframework.web.bind.annotation.RestController;
         produces = "application/vnd.ga-administrative+json;version=1.0"
 )
 public class AdministratorController {
+
+    private final AdministratorService service;
+
+    public AdministratorController(AdministratorService service) {
+        this.service = service;
+    }
+
+    @PostMapping
+    public ResponseEntity<AdministratorResponse> save(@RequestBody AdministratorRequest request) {
+        var response = service.save(request);
+        var uri = ServletUriComponentsBuilder
+                .fromCurrentContextPath()
+                .path("/{id}")
+                .buildAndExpand(response.id())
+                .toUri();
+        return ResponseEntity.created(uri).body(response);
+    }
 
 }
