ed5c8d077cf122da9bd388df9be16e376734b2e9
MauricioHidani <mauricio.hidani.work@gmail.com>
2024-03-06 16:51:05 -0300

    feat: create implamentation of administrator adapter

---
+++ backend/administrative/src/main/java/com/mh/ga/administrative/repositories/adapter/impls/AdministratorAdapterImpl.java
@@ -0,0 +1,18 @@
+package com.mh.ga.administrative.repositories.adapter.impls;
+
+import com.mh.ga.administrative.models.entities.Administrator;
+import com.mh.ga.administrative.repositories.adapter.AdministratorAdapter;
+
+import java.util.UUID;
+
+public class AdministratorAdapterImpl implements AdministratorAdapter<Administrator, UUID> {
+
+    @Override public Administrator save(Administrator administrator) {
+        return null;
+    }
+
+    @Override public Administrator findByDocument(String document) {
+        return null;
+    }
+
+}
