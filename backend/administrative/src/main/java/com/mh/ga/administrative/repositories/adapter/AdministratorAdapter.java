a5e7c78be0af377f72f4487120ad14af9c40ee76
MauricioHidani <mauricio.hidani.work@gmail.com>
2024-03-06 16:49:37 -0300

    feat: find by document administrator with adaptor

--- backend/administrative/src/main/java/com/mh/ga/administrative/repositories/adapter/AdministratorAdapter.java
+++ backend/administrative/src/main/java/com/mh/ga/administrative/repositories/adapter/AdministratorAdapter.java
@@ -2,4 +2,5 @@ package com.mh.ga.administrative.repositories.adapter;
 
 public interface AdministratorAdapter<ENTITY, IDENTITY> {
     ENTITY save(ENTITY entity);
+    ENTITY findByDocument(String document);
 }
