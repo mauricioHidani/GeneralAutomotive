c1492a65d7ac5bb21b386ed256711e2d01a4a939
MauricioHidani <mauricio.hidani.work@gmail.com>
2024-03-06 16:55:55 -0300

    feat: exception handler controller

---
+++ backend/administrative/src/main/java/com/mh/ga/administrative/controllers/handlers/ExceptionHandlerController.java
@@ -0,0 +1,8 @@
+package com.mh.ga.administrative.controllers.handlers;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ExceptionHandlerController {
+
+}
