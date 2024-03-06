536db5f0a274ba370451860bd6a8816b36a06fdf
MauricioHidani <mauricio.hidani.work@gmail.com>
2024-03-06 17:04:27 -0300

    feat: create bean IoC use case save administrator

--- backend/administrative/src/main/java/com/mh/ga/administrative/services/administrator/configs/AdministratorConfig.java
+++ backend/administrative/src/main/java/com/mh/ga/administrative/services/administrator/configs/AdministratorConfig.java
@@ -1,9 +1,13 @@
 package com.mh.ga.administrative.services.administrator.configs;
 
 import com.mh.ga.administrative.models.entities.Administrator;
+import com.mh.ga.administrative.models.transfers.AdministratorRequest;
+import com.mh.ga.administrative.models.transfers.AdministratorResponse;
 import com.mh.ga.administrative.repositories.AdministratorRepository;
 import com.mh.ga.administrative.repositories.adapter.AdministratorAdapter;
 import com.mh.ga.administrative.repositories.adapter.impls.AdministratorAdapterImpl;
+import com.mh.ga.administrative.services.administrator.SaveAdministrator;
+import com.mh.ga.administrative.services.administrator.impls.SaveAdministratorImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -16,6 +20,11 @@ public class AdministratorConfig {
     public AdministratorAdapter<Administrator, UUID> adapter(
             AdministratorRepository repository) {
         return new AdministratorAdapterImpl(repository);
+    }
+
+    @Bean public SaveAdministrator<AdministratorRequest, AdministratorResponse> save(
+            AdministratorAdapter<Administrator, UUID> adapter) {
+        return new SaveAdministratorImpl(adapter);
     }
 
 }
