b1b272aae5ccc55ecc4a73d317f50ae10f0c2d2b
MauricioHidani <mauricio.hidani.work@gmail.com>
2024-03-06 16:56:25 -0300

    feat: exception handler controller illegal argument exception

--- backend/administrative/src/main/java/com/mh/ga/administrative/controllers/handlers/ExceptionHandlerController.java
+++ backend/administrative/src/main/java/com/mh/ga/administrative/controllers/handlers/ExceptionHandlerController.java
@@ -1,8 +1,33 @@
 package com.mh.ga.administrative.controllers.handlers;
 
+import com.mh.ga.administrative.models.responses.StandardErrorResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.Clock;
+import java.time.Instant;
 
 @RestControllerAdvice
 public class ExceptionHandlerController {
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<StandardErrorResponse> illegalArgument(
+            IllegalArgumentException exception,
+            HttpServletRequest request
+    ) {
+        var status = HttpStatus.BAD_REQUEST;
+        var response = new StandardErrorResponse(
+                Instant.now(Clock.systemUTC()),
+                status.value(),
+                status.name(),
+                exception.getMessage(),
+                request.getRequestURI()
+        );
+
+        return ResponseEntity.status(status).body(response);
+    }
 
 }
