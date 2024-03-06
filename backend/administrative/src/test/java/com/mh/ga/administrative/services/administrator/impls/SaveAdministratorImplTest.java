47b0411e53a02820a38f80c2d57f808ed3ce8bc3
MauricioHidani <mauricio.hidani.work@gmail.com>
2024-03-06 16:59:18 -0300

    test: save administrator test

---
+++ backend/administrative/src/test/java/com/mh/ga/administrative/services/administrator/impls/SaveAdministratorImplTest.java
@@ -0,0 +1,9 @@
+package com.mh.ga.administrative.services.administrator.impls;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+class SaveAdministratorImplTest {
+
+}
