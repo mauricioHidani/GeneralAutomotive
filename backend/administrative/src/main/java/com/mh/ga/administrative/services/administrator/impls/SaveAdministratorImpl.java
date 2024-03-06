e20979026b55644683e49c58e3e7ee0dd25e1c97
MauricioHidani <mauricio.hidani.work@gmail.com>
2024-03-06 17:00:35 -0300

    feat: save administrator

---
+++ backend/administrative/src/main/java/com/mh/ga/administrative/services/administrator/impls/SaveAdministratorImpl.java
@@ -0,0 +1,14 @@
+package com.mh.ga.administrative.services.administrator.impls;
+
+import com.mh.ga.administrative.models.transfers.AdministratorRequest;
+import com.mh.ga.administrative.models.transfers.AdministratorResponse;
+import com.mh.ga.administrative.services.administrator.SaveAdministrator;
+
+public class SaveAdministratorImpl
+        implements SaveAdministrator<AdministratorRequest, AdministratorResponse> {
+
+    @Override public AdministratorResponse execute(AdministratorRequest request) {
+        return null;
+    }
+
+}
