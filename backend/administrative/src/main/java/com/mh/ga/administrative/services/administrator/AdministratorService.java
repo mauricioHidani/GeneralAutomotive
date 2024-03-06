a7d14d7bcabefba8ecf81a7b7818c9bc95bdc233
MauricioHidani <mauricio.hidani.work@gmail.com>
2024-03-06 17:06:25 -0300

    feat: administrator service use case save

--- backend/administrative/src/main/java/com/mh/ga/administrative/services/administrator/AdministratorService.java
+++ backend/administrative/src/main/java/com/mh/ga/administrative/services/administrator/AdministratorService.java
@@ -1,8 +1,23 @@
 package com.mh.ga.administrative.services.administrator;
 
+import com.mh.ga.administrative.models.transfers.AdministratorRequest;
+import com.mh.ga.administrative.models.transfers.AdministratorResponse;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class AdministratorService {
+
+    private final SaveAdministrator<AdministratorRequest, AdministratorResponse> save;
+
+    public AdministratorService(
+            SaveAdministrator<AdministratorRequest, AdministratorResponse> save) {
+        this.save = save;
+    }
+
+    @Transactional
+    public AdministratorResponse save(AdministratorRequest request) {
+        return save.execute(request);
+    }
 
 }
