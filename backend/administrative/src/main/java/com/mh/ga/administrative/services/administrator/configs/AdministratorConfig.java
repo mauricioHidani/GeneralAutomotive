693b415e8e74d3bdb680d05b66385be33b8524b6
MauricioHidani <mauricio.hidani.work@gmail.com>
2024-03-06 16:54:19 -0300

    feat: administrator config

---
+++ backend/administrative/src/main/java/com/mh/ga/administrative/services/administrator/configs/AdministratorConfig.java
@@ -0,0 +1,8 @@
+package com.mh.ga.administrative.services.administrator.configs;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AdministratorConfig {
+
+}
