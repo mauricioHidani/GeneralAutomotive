4b9a6f16e923b43fdc07baa220fe6bb55f777e44
MauricioHidani <mauricio.hidani.work@gmail.com>
2024-03-08 08:28:35 -0300

    feat: find administrator by id

--- backend/administrative/src/main/java/com/mh/ga/administrative/repositories/adapter/AdministratorAdapter.java
+++ backend/administrative/src/main/java/com/mh/ga/administrative/repositories/adapter/AdministratorAdapter.java
@@ -1,6 +1,9 @@
 package com.mh.ga.administrative.repositories.adapter;
 
+import java.util.Optional;
+
 public interface AdministratorAdapter<ENTITY, IDENTITY> {
     ENTITY save(ENTITY entity);
+    Optional<ENTITY> findById(IDENTITY id);
     ENTITY findByDocument(String document);
 }
