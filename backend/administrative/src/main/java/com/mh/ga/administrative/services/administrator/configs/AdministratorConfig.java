21243c08c5c36e23c385e10717dbf155a47c0057
MauricioHidani <mauricio.hidani.work@gmail.com>
2024-03-08 10:00:35 -0300

    feat: configured DI for find by id with spring bean

--- backend/administrative/src/main/java/com/mh/ga/administrative/services/administrator/configs/AdministratorConfig.java
+++ backend/administrative/src/main/java/com/mh/ga/administrative/services/administrator/configs/AdministratorConfig.java
@@ -6,7 +6,9 @@ import com.mh.ga.administrative.models.transfers.AdministratorResponse;
 import com.mh.ga.administrative.repositories.AdministratorRepository;
 import com.mh.ga.administrative.repositories.adapter.AdministratorAdapter;
 import com.mh.ga.administrative.repositories.adapter.impls.AdministratorAdapterImpl;
+import com.mh.ga.administrative.services.administrator.FindByIdAdministrator;
 import com.mh.ga.administrative.services.administrator.SaveAdministrator;
+import com.mh.ga.administrative.services.administrator.impls.FindByIdAdministratorImpl;
 import com.mh.ga.administrative.services.administrator.impls.SaveAdministratorImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -20,6 +22,11 @@ public class AdministratorConfig {
     public AdministratorAdapter<Administrator, UUID> adapter(
             AdministratorRepository repository) {
         return new AdministratorAdapterImpl(repository);
+    }
+
+    @Bean public FindByIdAdministrator<String, AdministratorResponse> findById(
+            AdministratorAdapter<Administrator, UUID> adapter) {
+        return new FindByIdAdministratorImpl(adapter);
     }
 
     @Bean public SaveAdministrator<AdministratorRequest, AdministratorResponse> save(
