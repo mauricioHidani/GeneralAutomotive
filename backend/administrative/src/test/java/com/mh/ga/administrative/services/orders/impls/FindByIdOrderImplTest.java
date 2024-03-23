8a0162395de48f443a073c6e87c0680a0fc83cf5
MauricioHidani <mauricio.hidani.work@gmail.com>
2024-03-23 09:10:17 -0300

    feat: create use case find order by id

---
+++ backend/administrative/src/test/java/com/mh/ga/administrative/services/orders/impls/FindByIdOrderImplTest.java
@@ -0,0 +1,87 @@
+package com.mh.ga.administrative.services.orders.impls;
+
+import com.mh.ga.administrative.models.entities.Order;
+import com.mh.ga.administrative.models.factories.OrderFactory;
+import com.mh.ga.administrative.models.transfers.OrderResponse;
+import com.mh.ga.administrative.repositories.adapter.OrderAdapter;
+import com.mh.ga.administrative.services.exceptions.ResourceNotFoundException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(SpringExtension.class)
+class FindByIdOrderImplTest {
+
+    private OrderAdapter<Order, UUID> adapter;
+    private FindByIdOrderImpl useCase;
+
+    private UUID existingId, nonExistingId, invalidId;
+    private Order entity;
+    private OrderResponse response;
+
+    @BeforeEach
+    void setUp() throws Exception {
+        adapter = mock(OrderAdapter.class);
+        useCase = new FindByIdOrderImpl(adapter);
+
+        existingId = UUID.randomUUID();
+        nonExistingId = UUID.randomUUID();
+        invalidId = null;
+        entity = OrderFactory.createEntityWithId(existingId);
+        response = OrderFactory.createResponseWithId(existingId);
+
+        when(adapter.findById(existingId)).thenReturn(Optional.of(entity));
+        when(adapter.findById(nonExistingId)).thenReturn(Optional.empty());
+    }
+
+    @Test
+    @DisplayName("FindById should return order response when successfull")
+    void findById_shouldReturnOrderResponse_whenSuccessfull() {
+        OrderResponse result = useCase.execute(existingId);
+
+        assertNotNull(result);
+        assertTrue(result instanceof OrderResponse);
+        assertEquals(response.id(), result.id());
+        assertEquals(response.status(), result.status());
+        assertEquals(response.description(), result.description());
+        assertEquals(response.registered(), result.registered());
+        assertEquals(response.liable(), result.liable());
+        assertTrue(result.inventory().containsAll(result.inventory()));
+    }
+
+    @Test
+    @DisplayName("FindByID should throw an IllegalArgumentException when the ID is null")
+    void findByID_shouldThrowAnIllegalArgumentException_whenTheIDIsNull() {
+        String expectedErrorMessage = "Operation not valid on the requested system";
+
+        Throwable result = assertThrows(
+                IllegalArgumentException.class,
+                () -> useCase.execute(invalidId)
+        );
+
+        assertEquals(expectedErrorMessage, result.getMessage());
+    }
+
+    @Test
+    @DisplayName("FindByID should throw a ResourceNotFoundException when Resource not found by ID")
+    void findByID_shouldThrowAResourceNotFoundException_whenResourceNotFoundByID() {
+        String expectedErrorMessage = "Order not found with the requested information";
+
+        Throwable result = assertThrows(
+                ResourceNotFoundException.class,
+                () -> useCase.execute(nonExistingId)
+        );
+
+        assertEquals(expectedErrorMessage, result.getMessage());
+    }
+
+}
