407910c5fb255e69f8e37854b81ce400864fa538
MauricioHidani <mauricio.hidani.work@gmail.com>
2024-03-25 17:17:00 -0300

    feat: create order adapter find by register and find all

--- backend/administrative/src/main/java/com/mh/ga/administrative/repositories/adapter/OrderAdapter.java
+++ backend/administrative/src/main/java/com/mh/ga/administrative/repositories/adapter/OrderAdapter.java
@@ -1,10 +1,17 @@
 package com.mh.ga.administrative.repositories.adapter;
 
+import ch.qos.logback.core.status.InfoStatus;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.time.Instant;
 import java.util.Optional;
 
 public interface OrderAdapter<ENTITY, IDENTITY> {
     boolean existingById(IDENTITY id);
     Optional<ENTITY> findById(IDENTITY id);
+    Page<ENTITY> findAll(Pageable pageable);
+    Page<ENTITY> findByRegistered(Instant start, Instant end, Pageable pageable);
     ENTITY save(ENTITY entity);
     void deleteById(IDENTITY id);
 }
