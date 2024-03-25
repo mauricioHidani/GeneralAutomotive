e8ed82f73d47468cc6fd554840e2be53dc85b761
MauricioHidani <mauricio.hidani.work@gmail.com>
2024-03-25 17:19:13 -0300

    feat: add in config, service and controller FindByRegistered use case

--- backend/administrative/src/main/java/com/mh/ga/administrative/services/orders/OrderService.java
+++ backend/administrative/src/main/java/com/mh/ga/administrative/services/orders/OrderService.java
@@ -2,24 +2,29 @@ package com.mh.ga.administrative.services.orders;
 
 import com.mh.ga.administrative.models.transfers.OrderRequest;
 import com.mh.ga.administrative.models.transfers.OrderResponse;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.UUID;
+import java.util.Map;
 
 @Service
 public class OrderService {
 
     private final FindByIdOrder<String, OrderResponse> findById;
+    private final FindByRegisterOrder<Map<String, String>, Pageable, Page<OrderResponse>> findByRegister;
     private final SaveOrder<OrderRequest, OrderResponse> save;
     private final UpdateOrder<OrderRequest, String, OrderResponse> update;
     private final DeleteOrder<String> delete;
 
     public OrderService(FindByIdOrder<String, OrderResponse> findById,
+                        FindByRegisterOrder<Map<String, String>, Pageable, Page<OrderResponse>> findByRegister,
                         SaveOrder<OrderRequest, OrderResponse> save,
                         UpdateOrder<OrderRequest, String, OrderResponse> update,
                         DeleteOrder<String> delete) {
         this.findById = findById;
+        this.findByRegister = findByRegister;
         this.save = save;
         this.update = update;
         this.delete = delete;
@@ -28,6 +33,11 @@ public class OrderService {
     @Transactional(readOnly = true)
     public OrderResponse findById(String id) {
         return findById.execute(id);
+    }
+
+    @Transactional(readOnly = true)
+    public Page<OrderResponse> findByRegister(Map<String, String> registers, Pageable pageable) {
+        return findByRegister.execute(registers, pageable);
     }
 
     @Transactional
